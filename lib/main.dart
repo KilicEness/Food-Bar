db6e012c36c7992c081d4a25436b9a572707b80f
enes <eneskiliicc06@gmail.com>
2024-01-04 20:01:07 +0300

    first page

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import './screens/main_tab/main_tab_screen.dart';
+
+void main() {
+  runApp(const FoodBar());
+}
+
+class FoodBar extends StatelessWidget {
+  const FoodBar({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Scan Your Food',
+      theme: ThemeData(
+        // Application theme data, you can set the colors for the application as
+        // you want
+        primarySwatch: Colors.green,
+      ),
+      // A widget which will be started on application startup
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MainTabScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/main_tab/main_tab_screen.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+class MainTabScreen extends StatefulWidget {
+  const MainTabScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainTabScreen> createState() => _MainTabScreenState();
+}
+
+class _MainTabScreenState extends State<MainTabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('FoodBar'),
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: const [
+                SizedBox(
+                  width: 20,
+                ),
+                Icon(Icons.fastfood),
+                Text(
+                  '  FOODBAR',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 250,
+              width: 320,
+              decoration: BoxDecoration(
+                color: Colors.lightGreen.shade900,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  bottomRight: Radius.circular(5.0),
+                ),
+              ),
+              child: Stack(
+                children: const [
+                  Positioned(
+                      top: 10,
+                      left: 10,
+                      child: Icon(Icons.food_bank_outlined,
+                          color: Colors.white, size: 50)),
+                  Positioned(
+                      top: 10,
+                      right: 10,
+                      child: Icon(
+                        Icons.free_breakfast_outlined,
+                        color: Colors.white,
+                        size: 60,
+                      )),
+                  Positioned(
+                    bottom: 15,
+                    left: 10,
+                    child: Text(
+                      "IF YOU WANT TO KNOW WHAT YOU EAT, \nYOU'RE IN THE RIGHT PLACE!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Icon(Icons.thumb_up_off_alt_outlined,
+                      color: Colors.black),
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Easy To Use',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      'Effortless marketing\nwith our intuitive and\neasy-to-use app.',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child:
+                      Icon(Icons.person_add_alt_1_rounded, color: Colors.black),
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'User Friendly',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      'Our app is designed to\nbe easily navigable for\nusers.',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Icon(Icons.verified_user_rounded, color: Colors.black),
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Metinleri sola hizala
+                  children: const [
+                    Text(
+                      'Trusted Contents',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      'We use reliable data from a\nworldwide open database.',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Ink(
+                height: 80,
+                width: 80,
+                decoration: ShapeDecoration(
+                  color: Colors.green.shade900,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.photo_camera_outlined,
+                      color: Colors.white, size: 50),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
